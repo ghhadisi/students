c8bfe2b12ef7302711e7fdb1b38d4ab2a837e19c
ssdeepin <ssdeepin@qq.com>
2019-01-28 22:10:58 +0800

    filters

---
+++ src/com/hss/student/filter/Filter2.java
@@ -0,0 +1,25 @@
+package com.hss.student.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Filter2 implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+//        System.err.println("过滤器创建了...");
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.err.println("来到了 过滤器 filter02  before");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.err.println("来到了 过滤器 filter02 after");
+    }
+
+    @Override
+    public void destroy() {
+//        System.err.println("过滤器销毁了...");
+
+    }
+}
---
+++ src/com/hss/student/filter/Filter3.java
@@ -0,0 +1,25 @@
+package com.hss.student.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class Filter3 implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+//        System.err.println("过滤器创建了...");
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.err.println("来到了 过滤器 filter03  before");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.err.println("来到了 过滤器 filter03 after");
+    }
+
+    @Override
+    public void destroy() {
+//        System.err.println("过滤器销毁了...");
+
+    }
+}
